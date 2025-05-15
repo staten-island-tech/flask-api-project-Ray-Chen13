55163dc41b50c6cb851e16081a36d7fa8c3cc661
Ray-Chen13 <rayc35@nycstudents.net>
2025-05-15 09:24:20 -0400

    eeeeeeeeeeeeeeeeeeee

--- app.py
+++ app.py
@@ -5,23 +5,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    response = response.get("https://www.amiiboapi.com/api/amiibo/?name=mario")
-    Mario_List = response.json()
+    response = requests.get("https://www.amiiboapi.com/api/amiibo/?name=mario")
+    data = response.json()
+    marios = []
 
-
-    Mario = []
-
-    for Mario in Mario_List:
-        Mario.append({
-            'amiiboSeries': Mario["amiiboSeries"].capitalize(),
-            'character': Mario["character"]
+    for mario in data.get('amiibo', []):
+        marios.append({
+            'series': mario['amiiboSeries'],
+            'character': mario['character'],
+            'id': mario['head'] + mario['tail'],  # Construct unique ID
+            'image': mario.get('image')
         })
 
-    return render_template("index.html", Mario = Mario)
+    return render_template("index.html", marios=marios)
 
-@app.route("/pokemon/<int:id>")
-def pokemon_detail(id):
-    response = requests.get(f"https://www.amiiboapi.com/api/amiibo/?name=mario")
-    Mario_List = response.json()
-    for Mario in Mario_List:
-        print(Mario)
+@app.route("/mario/<mario_id>")
+def mario_detail(mario_id):
+    response = requests.get("https://www.amiiboapi.com/api/amiibo/?name=mario")
+    data = response.json()
+
+    selected = None
+    for mario in data.get('amiibo', []):
+        id = mario['head'] + mario['tail']
+        if id == mario_id:
+            selected = mario
+            break
+
+    if selected:
+        return render_template("detail.html", mario=selected)
+    else:
+        return "Mario not found", 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
