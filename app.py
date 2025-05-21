e02ce430b6ca288be65285f5d2e26e700270251c
Ray-Chen13 <rayc35@nycstudents.net>
2025-05-21 09:24:15 -0400

    dsafnlkjgfh lkjh raelkjwsadhgxf

--- app.py
+++ app.py
@@ -5,39 +5,41 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # Fetch the data from the API
     response = requests.get("https://www.amiiboapi.com/api/amiibo/?name=mario")
     data = response.json()
     mario_list = data['amiibo']
 
     games = []
+    for i, game in enumerate(mario_list):
+        head = game['head']
+        tail = game['tail']
+        image_url = f"https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_{head}-{tail}.png"
 
-    for game in mario_list:
         games.append({
-            'amiiboSeries': game['amiiboSeries'].capitalize(),
-            'head': game['head'],
-            'tail': game['tail'],
+            'id': i,
+            'name': game['name'],
+            'image': image_url
         })
 
     return render_template("index.html", games=games)
-    
+
 @app.route("/pokemon/<int:id>")
-def game_detail(head):  
+def game_detail(id):
     response = requests.get("https://www.amiiboapi.com/api/amiibo/?name=mario")
     data = response.json()
+    game = data['amiibo'][id]
 
-    amiiboSeries = data.get('amiiboSeries').capitalize()
-    character = data.get('character)').capitalize()
-    name = data.get('name').capitalize()
-    tail = data.get('tail')
-    image_url = f"https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_{head}-{tail}.png"
-    image = image_url
+    image_url = f"https://raw.githubusercontent.com/N3evin/AmiiboAPI/master/images/icon_{game['head']}-{game['tail']}.png"
 
-    return render_template("mario.html", mario={
-        'amiiboSeries': amiiboSeries,
-        'character': character,
-        'image': image_url,
-        'name': name, 
-        'tail': tail
-    })
+    release_dates = {
+        'NA': game['release'].get('na', "N/A"),
+        'EU': game['release'].get('eu', "N/A"),
+        'JP': game['release'].get('jp', "N/A"),
+        'AU': game['release'].get('au', "N/A"),
+    }
+
+    return render_template("mario.html", game=game, image_url=image_url, release_dates=release_dates)
+
 if __name__ == '__main__':
     app.run(debug=True)
